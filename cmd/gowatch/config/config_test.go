e2a3effe75d7f8a19339de3fccbd1c158b942229
Matheus Alcantara <matheusssilv97@gmail.com>
2019-09-21 01:26:54 -0300

    tests: Create test to exported function LoadYml of package config

--- cmd/gowatch/config/config_test.go
+++ cmd/gowatch/config/config_test.go
@@ -1,11 +1,41 @@
 package config
 
-import "testing"
+import (
+	"testing"
+)
 
 var (
-	assertErrorMsg     = "Expected: %v; Got %v"
-	unexpectedErrorMsg = "Unexpected error: %v"
+	assertErrorMsg            = "Expected: %v; Got %v"
+	unexpectedErrorMsg        = "Unexpected error: %v"
+	keyDontLoadedCorrectlyMsg = "key %s don't loaded correctly"
 )
+
+func TestLoadYml(t *testing.T) {
+	ymlFile := "../testdata/gowatch.yml"
+	cfg, err := LoadYml(ymlFile)
+	if err != nil {
+		t.Fatalf(unexpectedErrorMsg, err)
+	}
+
+	if cfg.Verbose == false {
+		t.Errorf(keyDontLoadedCorrectlyMsg, "verbose")
+	}
+	if cfg.Dir != "." {
+		t.Errorf(keyDontLoadedCorrectlyMsg, "dir")
+	}
+
+	if len(cfg.Ignore) != 1 {
+		t.Errorf(keyDontLoadedCorrectlyMsg, "ignore")
+	}
+
+	if len(cfg.Buildflags) != 2 {
+		t.Errorf(keyDontLoadedCorrectlyMsg, "build_flags")
+	}
+
+	if len(cfg.RunFlags) != 2 {
+		t.Errorf(keyDontLoadedCorrectlyMsg, "run_flags")
+	}
+}
 
 func TestLoadYmlConfig(t *testing.T) {
 	ymlFile := "../testdata/gowatch.yml"
@@ -15,18 +45,18 @@ func TestLoadYmlConfig(t *testing.T) {
 		t.Fatalf(unexpectedErrorMsg, err)
 	}
 	if config.Dir == "" {
-		t.Fatalf("key dir don't loaded correctly")
+		t.Fatalf(keyDontLoadedCorrectlyMsg, "dir")
 	}
 	if len(config.Buildflags) == 0 {
-		t.Fatalf("key build_flags don't loaded correctly")
+		t.Fatalf(keyDontLoadedCorrectlyMsg, "build_flags")
 	}
 	if len(config.RunFlags) == 0 {
-		t.Fatalf("key run_flags don't loaded correctly")
+		t.Fatalf(keyDontLoadedCorrectlyMsg, "run_flags")
 	}
 	if len(config.Ignore) == 0 {
-		t.Fatalf("key ignore don't loaded correctly")
+		t.Fatalf(keyDontLoadedCorrectlyMsg, "ignore")
 	}
 	if config.Verbose == false {
-		t.Fatalf("key verbose don't loaded correctly")
+		t.Fatalf(keyDontLoadedCorrectlyMsg, "verbose")
 	}
 }
